876d55e232c0ddc7b7efa50cbe2f0fe10b2629c6
eatapple1004 <frank.virus0325@gmail.com>
2025-06-24 10:46:37 +0900

    .

--- services/analyze.service.js
+++ services/analyze.service.js
@@ -9,34 +9,14 @@ const logger = require("../utils/logger");
 
 const DEF_AI_DEV_URL = process.env.DEF_AI_DEV_URL;
 
+exports.checkExistService = async (videoID) => {
+
+}
+
 exports.sendAnalyzeRequest = async (url, userEmail) => {
 
     const userIdx = await usersRepository.getUserIdxByEmail(userEmail);
     logger.info(userEmail)
-    let platform = null;
-    let videoId = null;
-
-    logger.info("[1_2. analyze video] record analyze attemp:: " + videoId);
-
-    if (url.includes("youtube.com/shorts") || url.includes("youtube")) {
-        platform = "YouTube";
-        const match = url.match(/(?:shorts\/|watch\?v=|youtu\.be\/)([\w-]+)/);
-        videoId = match ? match[1] : null;
-    } else if (url.includes("instagram.com/reel")) {
-        platform = "Instagram";
-        const match = url.match(/instagram\.com\/reel\/([a-zA-Z0-9_-]+)/);
-        videoId = match ? match[1] : null;
-    } else if (url.includes("tiktok.com")) {
-        platform = "TikTok";
-        const match = url.match(/video\/(\d+)/);
-        videoId = match ? match[1] : null;
-    } else {
-        throw new Error("지원되지 않는 플랫폼입니다.");
-    }
-
-    if (!videoId) {
-        throw new Error("영상 ID를 추출할 수 없습니다.");
-    }
 
     // 영상 분석 요청 db 저장
     const requestIdx = await analyzeRepository.insertAnalyzeRequest({
