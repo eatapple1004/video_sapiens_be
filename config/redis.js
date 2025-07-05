6c5eba3552fea9202c857ff8e577572ae3e9a6dc
eatapple1004 <frank.virus0325@gmail.com>
2025-07-05 16:39:21 +0900

    set redis num

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,15 @@
 const redis = require("redis");
 const logger = require("../utils/logger");
 
-const client = redis.createClient();
+//const client = redis.createClient();
+
+const client = redis.createClient({
+  socket: {
+    host: '127.0.0.1',
+    port: 6379,
+  },
+  database: 0  // ✅ DB 번호 설정
+});
 
 client.connect()
   .then(() => logger.info("✅ Redis 연결 성공"))
