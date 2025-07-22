76dcb6b4eb51ac62daabe69c0d67239833f03189
eatapple1004 <frank.virus0325@gmail.com>
2025-07-22 09:32:49 +0900

    handler error solved

--- routers/search.router.js
+++ routers/search.router.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const searchController = require('../controllers/analyze.controller');
+const searchController = require("../controllers/search.controller");
 //const { authenticate } = require("../middlewares/authMiddleware");
 
 
 //진행중
 router.get("/search/integrated", searchController.integreatedSearch);
-router.get("/search/tag", searchController.tagSearch);
+router.get("/search/tag",        searchController.tagSearch);
 
 //진행 예정
-router.get("/search/creator");
+//router.get("/search/creator");
 
 module.exports = router;
