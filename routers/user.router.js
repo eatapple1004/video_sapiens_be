3997f8e11a1cdfff9a90df33cba5938579b3c3f0
eatapple1004 <frank.virus0325@gmail.com>
2025-08-05 09:10:48 +0900

    user router refactorying

--- routers/user.router.js
+++ routers/user.router.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 const { authenticate } = require("../middlewares/authMiddleware");
 
+// register
 router.post("/send-otp",      userController.sendOtp);
 router.post("/verify-otp",    userController.verifyOtp);
 router.post("/user/register", userController.registerUser);
 
+// login
 router.post("/user/login",    userController.loginUser);
 
-router.get("/username", authenticate, userController.getTokenUsername);
+// 얜 뭐였지?
+router.get("/username",   authenticate, userController.getTokenUsername);
 
+// 유저 마킹 기능
 router.post("/user/mark", authenticate, userController.markVideo);
 
 module.exports = router;
